124e29a36b01c2faebc194e6f7d6c2c6c29808f7
Terence <16819@ma-web.nl>
2014-12-09 11:32:13 +0100

    Wrap

--- app/views/home.php
+++ app/views/home.php
@@ -4,6 +4,18 @@
         <h1>Home</h1>
     </header>
     <!-- /Home (Head) -->
-
-    
+    <div class="wrap">
+		<div class="group">
+			<h2>Timery</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, pariatur! Alias aspernatur, animi adipisci amet similique ipsam, incidunt laborum totam!</p>
+		</div>
+		<div class="group">
+			<h2>Pickory</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est iure veritatis sunt, vel alias explicabo minima itaque soluta repudiandae sapiente!</p>
+		</div>
+		<div class="group">
+			<h2>Groupery</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae dolorem veritatis ipsum fuga, quisquam eius et voluptatem consectetur odit magni?</p>
+		</div>
+    </div>
 </main>
