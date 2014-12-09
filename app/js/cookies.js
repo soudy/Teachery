d0080a895334a300e0c2710bedb9521dc6c833c6
steven <stevenoud@hotmail.nl>
2014-12-10 00:00:49 +0100

    added license

--- app/js/cookies.js
+++ app/js/cookies.js
@@ -1,8 +1,19 @@
 /*
- * cookies.js
- * Copyright (C) 2014 soud
- *
- * Distributed under terms of the MIT license.
+ * Teachery is a web application to make the life of teacher's easier.
+ * Copyright (C) 2014 Terence Keur, Mirko van der Waal and Steven Oud
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 function Cookies()
--- app/js/render.js
+++ app/js/render.js
@@ -1,7 +1,23 @@
-if ( !window.requestAnimationFrame ) {
- 
-	window.requestAnimationFrame = ( function() {
- 
+/*
+ * Teachery is a web application to make the life of teacher's easier.
+ * Copyright (C) 2014 Terence Keur, Mirko van der Waal and Steven Oud
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+if (!window.requestAnimationFrame) {
+	window.requestAnimationFrame = (function() {
 		return window.webkitRequestAnimationFrame ||
 		window.mozRequestAnimationFrame ||
 		window.oRequestAnimationFrame ||
@@ -9,9 +25,7 @@ if ( !window.requestAnimationFrame ) {
 		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
 			window.setTimeout( callback, 1000 / 60 );
 		};
- 
-	} )();
- 
+	})();
 }
 
 function Render(settings){
