67d2878ae2627858ded29cdae7b6b991ce3f0819
Imakethings <Mirkovdw@outlook.com>
2014-12-09 15:06:00 +0100

    Landing :)

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Teachery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app/style.css">
+</head>
+<body>
+    <div class="landing">
+        <img src="teachery.svg" alt="" >
+        <div>
+            <a href="app">CONTINUE</a>
+        </div>
+    </div>
+</body>
+</html>
