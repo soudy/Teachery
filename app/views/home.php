7adefe330b77c649865786e08da4f1270c535b98
Imakethings <Mirkovdw@outlook.com>
2014-12-09 14:06:24 +0100

    Names woop

--- app/views/home.php
+++ app/views/home.php
@@ -4,16 +4,16 @@
         <h1>Home</h1>
     </header>
     <!-- /Home (Head) -->
-    <div class="wrap">
-		<div class="group">
+    <div class="home">
+		<div class="home-group">
 			<h2><i class="icon ion-clock"></i> Timery</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, pariatur! Alias aspernatur, animi adipisci amet similique ipsam, incidunt laborum totam!</p>
 		</div>
-		<div class="group">
+		<div class="home-group">
 			<h2><i class="icon ion-person"></i> Pickory</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est iure veritatis sunt, vel alias explicabo minima itaque soluta repudiandae sapiente!</p>
 		</div>
-		<div class="group">
+		<div class="home-group">
 			<h2><i class="icon ion-person-stalker"></i> Groupery</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae dolorem veritatis ipsum fuga, quisquam eius et voluptatem consectetur odit magni?</p>
 		</div>
