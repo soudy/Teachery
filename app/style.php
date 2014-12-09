c96891b1b866ef7297d1004b3fbc5e8197fe17e8
steven <stevenoud@hotmail.nl>
2014-12-09 13:53:37 +0100

    COOKIE FIX WOOHOO

--- app/style.php
+++ app/style.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 //setcookie("BASECOLOR", '#ff00ff', time()+60);
 
@@ -6,11 +6,11 @@ $content = file_get_contents('style.css');
 
 // ICON_COLOR: #F2F2F2;
 // BACKGROUND: #821031;
-// TEXT_COLOR: #191919; 
+// TEXT_COLOR: #191919;
 $colors = array(
-    array('#821031', 'BASECOLOR'),
-    array('#F2F2F2', 'ICON_COLOR'),
-    array('#191919', 'TEXT_COLOR'),
+    array('#821031', 'base_color'),
+    array('#F2F2F2', 'icon_color'),
+    array('#F2F2F2', 'background_color'),
 );
 
 foreach ($colors as $value) {
