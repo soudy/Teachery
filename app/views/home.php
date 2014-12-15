a5aacc7f43643825f290798b339f90c6a78e33a0
steven <stevenoud@hotmail.nl>
2014-12-15 12:32:34 +0100

    Pickery instructions

--- app/views/home.php
+++ app/views/home.php
@@ -7,11 +7,20 @@
     <div class="home">
 		<div class="home-group">
 			<h2><i class="icon ion-clock"></i> Timery</h2>
-			<p>With Timery you can create as many timers as you want, each with different sound, duration, name and more.</p>
+			<p>With Timery you can create as many timers as you want, each with different duration, name and more.</p>
 		</div>
 		<div class="home-group">
 			<h2><i class="icon ion-person"></i> Pickery</h2>
-			<p>Pickery allows you to easily select a random name from a list of names. You can export a class as CSV in Magister and import it here, and it will be ready for use.</p>
+            <p>Pickery allows you to easily select a random name from a list of names. You can export a class as CSV in Magister and import it here by doing the following steps:<br />
+                <ol>
+                    <li><a href="https://mediacollege.swp.nl/5.6.34/magister.aspx">Log into Magister.</a></li>
+                    <li>Go to the class you want to load in.</li>
+                    <li>Click on "Export" above.</li>
+                    <li>Choose the "comma seperated text" option.</li>
+                    <li>Go to Pickery and import the file you just exported from Magsiter. </li>
+                    <li>Done! You can now start generating random names.</li>
+                </ol>
+            </p>
 		</div>
     </div>
 </main>
