4cee2195d3fb0bb5b5863f67d94b091637c5598b
Imakethings <Mirkovdw@outlook.com>
2015-01-13 16:44:39 +0100

    Re-added clocks ._.

--- app/views/time.php
+++ app/views/time.php
@@ -56,6 +56,7 @@
     <!-- Timer + -->
     <div class="checkbox-plus">
         <a class="addTimer" href="#AddTimer"><i class="icon ion-plus"></i></a>
+        <a class="muteSounds muted" href="#MuteSound"><i class="icon ion-volume-mute"></i></a> <!-- <i class="icon ion-volume-high"></i> -->
         <a class="removeTimers" href="#RemoveTimers"><i class="icon ion-trash-a"></i></a>
     </div>
     <!-- /Timer + -->
